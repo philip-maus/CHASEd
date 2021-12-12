37089641372b877c87afabb6e3970275ee572b07
Philip Maus <2-philip@users.noreply.git.pmaus.de>
2021-12-12 15:48:00 +0000

    CI/CD

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         clean: true
     },
     cache: {
